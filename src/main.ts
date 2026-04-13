1f050ea3687b66417d27daa43a4fc5d4a8c559f9
miguel-cardoso08k <joaomiguel.cardoso2006@outlook.com>
2026-04-12 23:54:12 -0300

    fix: enable global request validation

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,20 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
   );
 
   const port = Number(process.env.PORT ?? 3000);
